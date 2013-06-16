77f4140b825e29f67f91ae27ed73c53e808f5ba0
Alexander <skiba-alex@ya.ru>
2013-06-17 01:25:10 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cloudpatrol::Application.config.secret_key_base = '17539122ccd7e67f6b159d288101b87b70061bb45ac7da278ccd21bccac9647192ecd8b66eae3b1667badaf521e7480bf3420f9a41c1021c0e3ad8126b7abd1f'
