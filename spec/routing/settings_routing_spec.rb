761687f8b59127bc9406707f9e58aa6ec31fbd3a
Alexander <skiba-alex@ya.ru>
2013-06-18 17:57:00 +0700

    Update specs

--- spec/routing/settings_routing_spec.rb
+++ spec/routing/settings_routing_spec.rb
@@ -2,17 +2,12 @@ require "spec_helper"
 
 describe SettingsController do
   describe "routing" do
-
     it "routes to #index" do
       get("/settings").should route_to("settings#index")
     end
 
     it "routes to #new" do
       get("/settings/new").should route_to("settings#new")
-    end
-
-    it "routes to #show" do
-      get("/settings/1").should route_to("settings#show", :id => "1")
     end
 
     it "routes to #edit" do
@@ -30,6 +25,5 @@ describe SettingsController do
     it "routes to #destroy" do
       delete("/settings/1").should route_to("settings#destroy", :id => "1")
     end
-
   end
 end
--- spec/views/settings/index.html.haml_spec.rb
+++ spec/views/settings/index.html.haml_spec.rb
@@ -4,20 +4,20 @@ describe "settings/index" do
   before(:each) do
     assign(:settings, [
       stub_model(Setting,
-        :key => "Key",
-        :value => "Value"
+        key: "key_1",
+        value: "value"
       ),
       stub_model(Setting,
-        :key => "Key",
-        :value => "Value"
+        key: "key_2",
+        value: "value"
       )
     ])
   end
 
   it "renders a list of settings" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Key".to_s, :count => 2
-    assert_select "tr>td", :text => "Value".to_s, :count => 2
+    assert_select "input", text: "key_1".to_s
+    assert_select "input", text: "key_2".to_s
+    assert_select "input", text: "value".to_s, count: 2
   end
 end
