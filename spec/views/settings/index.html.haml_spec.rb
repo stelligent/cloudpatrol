16b1b3d66c102b52b9534bf70ab58946dae1f65d
Alexander <skiba-alex@ya.ru>
2013-07-11 04:15:00 +0700

    Update settings index view spec

--- spec/views/settings/index.html.haml_spec.rb
+++ spec/views/settings/index.html.haml_spec.rb
@@ -5,22 +5,29 @@ describe "settings/index" do
     assign(:settings, [
       stub_model(Setting,
         key: "key_1",
-        value: "value",
-        protected: false
+        value: "value_1",
+        protected: "none"
       ),
       stub_model(Setting,
         key: "key_2",
-        value: "value",
-        protected: true
+        value: "value_2",
+        protected: "key"
+      ),
+      stub_model(Setting,
+        key: "key_3",
+        value: "value_3",
+        protected: "both"
       )
     ])
   end
 
   it "renders a list of settings" do
     render
-    assert_select "input", value: "key_1"
+    assert_select "input", value: "key_1", disabled: false
+    assert_select "input", value: "value_1", disabled: false
     assert_select "input", value: "key_2", disabled: true
-    assert_select "input", value: "value"
-    assert_select "input", value: "value", disabled: true
+    assert_select "input", value: "value_2", disabled: false
+    assert_select "input", value: "key_3", disabled: true
+    assert_select "input", value: "value_3", disabled: true
   end
 end
