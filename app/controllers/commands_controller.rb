d6192fee36714a696989997f48c280a928f59c5a
Alexander <skiba-alex@ya.ru>
2013-07-11 04:39:00 +0700

    Improve UI

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -3,7 +3,11 @@ class CommandsController < ApplicationController
     @settings_arr = Setting.all.map { |s| [s.key.to_sym, s.value] }
     @settings = {}
     @settings_arr.each do |s|
-      @settings[s[0]] = s[1]
+      @settings[s[0]] = s[1] if s[1].present?
+    end
+    unless @settings.keys.include?(:aws_access_key_id) and @settings.keys.include?(:aws_secret_access_key)
+      flash.now[:alert] = "AWS credentials are missing!"
+      @disable_commands = true
     end
   end
 end
--- app/helpers/commands_helper.rb
+++ app/helpers/commands_helper.rb
@@ -2,9 +2,9 @@ module CommandsHelper
   def aws_setting key
     link_to(
     if @settings[key].present?
-      content_tag(:span, "#{key} = #{@settings[key]}", title: key.to_s)
+      content_tag(:span, "#{key} = #{@settings[key]}", title: key.to_s, class: "aws-setting")
     else
-      content_tag(:span, "#{key}", class: "undefined")
+      content_tag(:span, "#{key}", class: "aws-setting undefined")
     end, settings_path)
   end
 end
