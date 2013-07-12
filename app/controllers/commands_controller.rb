5ad674692c0a875467e9bb54465e3db858132430
Alexander <skiba-alex@ya.ru>
2013-07-13 03:12:05 +0700

    Update CommandsController#perform method

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -10,12 +10,16 @@ class CommandsController < ApplicationController
   end
 
   def perform
-    if @command
-      # command runs
-      klass = Cloudpatrol::Task.const_get(@command[:class])
-      if klass.new(creds).try(@command[:method])
-        flash.now[:notice] = "#{@command} was executed"
+    if @command and creds
+      args = retrieve_args_for(@command)
+
+      request = if args
+        Cloudpatrol.perform(creds, @command[:class], @command[:method], args)
+      else
+        Cloudpatrol.perform(creds, @command[:class], @command[:method])
       end
+
+      flash.now[:notice] = "#{@command} was executed"
     else
       flash.now[:alert] = "Wrong, wrong."
     end
@@ -25,21 +29,50 @@ class CommandsController < ApplicationController
 private
 
   def discover_command
-    @command =
+    @command = begin
       if params[:class] and params[:method] and Cloudpatrol::Task.const_get(params[:class].to_sym).public_instance_methods(false).include?(params[:method].to_sym)
         {
           class: params[:class].to_sym,
           method: params[:method].to_sym
         }
       else
-        nil
+        raise
       end
+    rescue
+      nil
+    end
+  end
+
+  def retrieve_args_for command
+    arg = case command[:class]
+    when :EC2
+      "ec2_instance_age" if command[:method] = :clean_instances
+    when :OpsWorks
+      case command[:method]
+      when :clean_stacks
+        "opsworks_stack_age"
+      when :clean_layers
+        "opsworks_layer_age"
+      when :clean_instances
+        "opsworks_instance_age"
+      when :clean_apps
+        "opsworks_app_age"
+      end
+    when :CloudFormation
+      "cloudformation_stack_age" if command[:method] = :clean_stacks
+    end
+    return Setting.find_by_key(arg).try(:value) if arg
+    nil
   end
 
   def creds
-    {
-      access_key_id: Setting.find_by_name("aws_access_key_id"),
-      secret_access_key: Setting.find_by_name("aws_secret_access_key")
-    }
+    if access_key_id = Setting.find_by_key("aws_access_key_id").value and secret_access_key = Setting.find_by_key("aws_secret_access_key").value
+      {
+        access_key_id: access_key_id,
+        secret_access_key: secret_access_key
+      }
+    else
+      nil
+    end
   end
 end
