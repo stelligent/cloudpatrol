5f24f2131284ef2ae4f51f11fa418a4f78a7ca65
Alexander <skiba-alex@ya.ru>
2013-07-15 00:39:45 +0700

    Add default value for table

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ User.create(name: "admin", password: "admin", password_confirmation: "admin")
 %w{ aws_access_key_id aws_secret_access_key dynamodb_log_table ec2_instance_age opsworks_stack_age opsworks_layer_age opsworks_instance_age opsworks_app_age cloudformation_stack_age }.each do |setting|
   Setting.create(key: setting, value: "", protected: "key")
 end
+
+Setting.find_by_key("dynamodb_log_table").update_column(:value, "cloudpatrol-log")
