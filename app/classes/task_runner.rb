f84d6eabd7457f4016526f9150dd6d14808e2cb3
Alexander <skiba-alex@ya.ru>
2013-07-19 10:49:32 +0700

    Refactor TaskRunner a bit

--- app/classes/task_runner.rb
+++ app/classes/task_runner.rb
@@ -1,45 +1,46 @@
 class TaskRunner
-  def initialize command
-    @command = command
+  def initialize command = {}
+    @command = {
+      class: command[:class].to_sym,
+      method: command[:method].to_sym
+    }
     @creds = creds
   end
 
   def run
-    begin
-      if @creds and Cloudpatrol::Task.const_get(@command[:class].to_sym).public_instance_methods(false).include?(@command[:method].to_sym)
-        request_args = [ @creds, log_table_name, @command[:class], @command[:method] ]
-        task_arg = fetch_arg
-        unless task_arg.nil?
-          request_args << task_arg
-        end
-        request = Cloudpatrol.perform(*request_args)
-      else
-        raise
+    if @creds and Cloudpatrol::Task.const_get(@command[:class]).public_instance_methods(false).include?(@command[:method])
+      request_args = [ @creds, log_table_name, @command[:class], @command[:method] ]
+      task_arg = fetch_arg
+      unless task_arg.nil?
+        request_args << task_arg
       end
-    rescue
-      false
+      request = Cloudpatrol.perform(*request_args)
+    else
+      raise
     end
+  rescue
+    false
   end
 
 private
 
   def fetch_arg
     arg = case @command[:class]
-    when "EC2"
-      "ec2_instance_age" if @command[:method] == "clean_instances"
-    when "OpsWorks"
+    when :EC2
+      "ec2_instance_age" if @command[:method] == :clean_instances
+    when :OpsWorks
       case @command[:method]
-      when "clean_stacks"
+      when :clean_stacks
         "opsworks_stack_age"
-      when "clean_layers"
+      when :clean_layers
         "opsworks_layer_age"
-      when "clean_instances"
+      when :clean_instances
         "opsworks_instance_age"
-      when "clean_apps"
+      when :clean_apps
         "opsworks_app_age"
       end
     when :CloudFormation
-      "cloudformation_stack_age" if @command[:method] == "clean_stacks"
+      "cloudformation_stack_age" if @command[:method] == :clean_stacks
     end
 
     if arg
