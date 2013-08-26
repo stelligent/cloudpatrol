c066a7619083b3140dd3e16abd6ceb696c2fea03
Alexander Skiba <skiba-alex@ya.ru>
2013-08-26 18:05:33 +0700

    Perform EC2#clean_instances after CloudFormation

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,14 +22,15 @@ end
 
 # Crontab contents
 every :day, at: run_at do
-  runner 'TaskRunner.new(class: "EC2", method: "clean_instances").run'
-  runner 'TaskRunner.new(class: "EC2", method: "clean_security_groups").run'
-  runner 'TaskRunner.new(class: "EC2", method: "clean_ports_in_default").run'
-  runner 'TaskRunner.new(class: "EC2", method: "clean_elastic_ips").run'
-  runner 'TaskRunner.new(class: "IAM", method: "clean_users").run'
   runner 'TaskRunner.new(class: "OpsWorks", method: "clean_apps").run'
   runner 'TaskRunner.new(class: "OpsWorks", method: "clean_instances").run'
   runner 'TaskRunner.new(class: "OpsWorks", method: "clean_layers").run'
   runner 'TaskRunner.new(class: "OpsWorks", method: "clean_stacks").run'
   runner 'TaskRunner.new(class: "CloudFormation", method: "clean_stacks").run'
+  # EC2#clean_instances should run after CloudFormation
+  runner 'TaskRunner.new(class: "EC2", method: "clean_instances").run'
+  runner 'TaskRunner.new(class: "EC2", method: "clean_security_groups").run'
+  runner 'TaskRunner.new(class: "EC2", method: "clean_ports_in_default").run'
+  runner 'TaskRunner.new(class: "EC2", method: "clean_elastic_ips").run'
+  runner 'TaskRunner.new(class: "IAM", method: "clean_users").run'
 end
