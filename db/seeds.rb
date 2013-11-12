0aab1b6797830e5c6b169253e0b6501ae12e5197
Eric Kascic <eric.kascic@stelligent.com>
2013-11-12 09:34:07 -0500

    try to make this idempotent so it can be called over and over again from opsworks deploy event

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-User.create(name: "admin", password: "admin", password_confirmation: "admin")
+admin_user = User.find(name: "admin")
+if admin_user.nil?
+  User.create(name: "admin", password: "admin", password_confirmation: "admin")
 
-Setting.create(key: "aws_access_key_id", value: "", protected: "key")
-Setting.create(key: "aws_secret_access_key", value: "", protected: "key", masked: true)
-Setting.create(key: "dynamodb_log_table", value: "cloudpatrol-log", protected: "key")
+  Setting.create(key: "aws_access_key_id", value: "", protected: "key")
+  Setting.create(key: "aws_secret_access_key", value: "", protected: "key", masked: true)
+  Setting.create(key: "dynamodb_log_table", value: "cloudpatrol-log", protected: "key")
 
-%w{ ec2_instance_age opsworks_stack_age opsworks_layer_age opsworks_instance_age opsworks_app_age cloudformation_stack_age rules_run_at }.each do |setting|
-  Setting.create(key: setting, value: "", protected: "key")
+  %w{ ec2_instance_age opsworks_stack_age opsworks_layer_age opsworks_instance_age opsworks_app_age cloudformation_stack_age rules_run_at }.each do |setting|
+    Setting.create(key: setting, value: "", protected: "key")
+  end
 end
