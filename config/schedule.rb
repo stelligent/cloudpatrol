6090702755cb094542734d24b119b59ca7f38e74
Eric Kascic <eric.kascic@stelligent.com>
2013-11-20 10:19:40 -0500

    remove dead commented out code from schedule.rb remove calls to more dangerous commands for now until a better disable/enable mechanism can be fashioned permanently remove calls to opsworks commands to mess with layers/apps/instances

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,28 +9,15 @@ begin
 rescue ArgumentError, TypeError
   raise ArgumentError, "rules_run_at is in the wrong format"
 end
-# begin
-#   start_time = Time.parse(@ec2_instances_start_time)
-# rescue ArgumentError, TypeError
-#   raise ArgumentError, "ec2_instances_start_time is in the wrong format"
-# end
-# begin
-#   stop_time = Time.parse(@ec2_instances_stop_time)
-# rescue ArgumentError, TypeError
-#   raise ArgumentError, "ec2_instances_stop_time is in the wrong format"
-# end
 
 # Crontab contents
 every :day, at: run_at do
-  runner 'TaskRunner.new(class: "OpsWorks", method: "clean_apps").run'
-  runner 'TaskRunner.new(class: "OpsWorks", method: "clean_instances").run'
-  runner 'TaskRunner.new(class: "OpsWorks", method: "clean_layers").run'
   runner 'TaskRunner.new(class: "OpsWorks", method: "clean_stacks").run'
   runner 'TaskRunner.new(class: "CloudFormation", method: "clean_stacks").run'
   # EC2#clean_instances should run after CloudFormation
   runner 'TaskRunner.new(class: "EC2", method: "clean_instances").run'
-  runner 'TaskRunner.new(class: "EC2", method: "clean_security_groups").run'
+  #runner 'TaskRunner.new(class: "EC2", method: "clean_security_groups").run'
   runner 'TaskRunner.new(class: "EC2", method: "clean_ports_in_default").run'
   runner 'TaskRunner.new(class: "EC2", method: "clean_elastic_ips").run'
-  runner 'TaskRunner.new(class: "IAM", method: "clean_users").run'
+  #runner 'TaskRunner.new(class: "IAM", method: "clean_users").run'
 end
