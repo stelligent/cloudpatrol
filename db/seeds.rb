465361e5891fa3a21213cfca5fe0a17a4eddfce7
Eric Kascic <eric.kascic@stelligent.com>
2013-11-20 07:55:40 -0500

    remove any opsworks code that applies only to apps/layers/instance.  stacks are the unit of collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ if admin_user.nil?
   Setting.create(key: "aws_secret_access_key", value: "", protected: "key", masked: true)
   Setting.create(key: "dynamodb_log_table", value: "cloudpatrol-log", protected: "key")
 
-  %w{ ec2_instance_age opsworks_stack_age opsworks_layer_age opsworks_instance_age opsworks_app_age cloudformation_stack_age rules_run_at }.each do |setting|
+  %w{ ec2_instance_age opsworks_stack_age cloudformation_stack_age rules_run_at }.each do |setting|
     Setting.create(key: setting, value: "", protected: "key")
   end
 
