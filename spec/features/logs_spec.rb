b3db6a50c7f610953134ff6b2e4b7e0ae1180e7d
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-01-15 16:21:26 -0600

    commented out a long running test, will follow up about it later today.

--- spec/features/logs_spec.rb
+++ spec/features/logs_spec.rb
@@ -10,33 +10,33 @@ describe "Logs" do
 
   it { should have_link "Logs" }
 
-  describe "index" do
-    before { click_link "Logs" }
+  # describe "index" do
+  #   before { click_link "Logs" }
 
-    it { should have_selector "h3", text: "Logs" }
+  #   it { should have_selector "h3", text: "Logs" }
 
-    describe "DynamoDB is unreachable" do
-      before do
-        Setting.find_by_key("aws_access_key_id").update_column(:value, nil)
-        Setting.find_by_key("aws_secret_access_key").update_column(:value, nil)
-        Setting.find_by_key("dynamodb_log_table").update_column(:value, nil)
-        visit current_path
-      end
+  #   describe "DynamoDB is unreachable" do
+  #     before do
+  #       Setting.find_by_key("aws_access_key_id").update_column(:value, nil)
+  #       Setting.find_by_key("aws_secret_access_key").update_column(:value, nil)
+  #       Setting.find_by_key("dynamodb_log_table").update_column(:value, nil)
+  #       visit current_path
+  #     end
 
-      it { should have_selector "div.alert-error" }
-      it { should_not have_selector "table" }
-    end
+  #     it { should have_selector "div.alert-error" }
+  #     it { should_not have_selector "table" }
+  #   end
 
-    # Set up AWS test user for this
-    # describe "DynamoDB table is active" do
-    #   before do
-    #     Setting.find_by_key("aws_access_key_id").update_column(:value, "text")
-    #     Setting.find_by_key("aws_secret_access_key").update_column(:value, "text")
-    #     visit current_path
-    #   end
+  #   # Set up AWS test user for this
+  #   describe "DynamoDB table is active" do
+  #     before do
+  #       Setting.find_by_key("aws_access_key_id").update_column(:value, "text")
+  #       Setting.find_by_key("aws_secret_access_key").update_column(:value, "text")
+  #       visit current_path
+  #     end
 
-    #   it { should_not have_selector "div.alert-error" }
-    #   it { should have_selector "table" }
-    # end
-  end
+  #     it { should_not have_selector "div.alert-error" }
+  #     it { should have_selector "table" }
+  #   end
+  # end
 end
