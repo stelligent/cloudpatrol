0382fb246ad22fb859cef814872e54d84500f38c
jonny sywulak <jonny@stelligent.com>
2015-05-03 22:35:46 -0500

    don't try to kill the server if the pid dir doesn't existing (indicative that this is the first deployment)

--- scripts/applicationStop.sh
+++ scripts/applicationStop.sh
@@ -1,2 +1,4 @@
-# export pid=`cat /cloudpatrol/tmp/pids/server.pid`
-# kill -9 $pid
+if [ -d "/cloudpatrol/tmp/pids/" ]; then
+  export pid=`cat /cloudpatrol/tmp/pids/server.pid`
+  kill -9 $pid
+fi
